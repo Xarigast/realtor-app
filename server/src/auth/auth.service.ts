9b14e08082a0f8ddc85ce2c9d7800a9a7fa5736c
Xarigast <xarigast@gmail.com>
2023-08-11 12:11:05 +0300

    server: create jwt strategy, create sign-in, sign-up methods in auth service

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+import { User } from './user.entity';
+
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
+
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDTO,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.usersRepository.findOneBy({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { username };
+
+      const accessToken: string = await this.jwtService.signAsync(payload, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Incorrect login or password.');
+    }
+  }
+
+  async signUp(
+    authCredentialsDto: AuthCredentialsDTO,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = this.usersRepository.create({
+      username,
+      password: hashedPassword,
+    });
+
+    try {
+      await this.usersRepository.save(newUser);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username is already taken.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+
+    return this.signIn(authCredentialsDto);
+  }
+}
---
+++ server/src/auth/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from './user.entity';
+
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {
+    super({
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+
+    const user: User = await this.usersRepository.findOneBy({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
