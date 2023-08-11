405463dfdc800aebefaaaaa180016938f5c09a4d
Xarigast <xarigast@gmail.com>
2023-08-11 12:11:58 +0300

    server: apply auth service methods in auth controller

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/sign-up')
+  signUp(
+    @Body() authCredentialsDto: AuthCredentialsDTO,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/sign-in')
+  signIn(
+    @Body() authCredentialsDto: AuthCredentialsDTO,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
+  }
+}
