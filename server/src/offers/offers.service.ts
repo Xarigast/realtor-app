af6aa7f448246475f0e622d3b1c78ed81f42742f
Xarigast <xarigast@gmail.com>
2023-08-11 11:41:13 +0300

    server: create offers service methods

--- server/src/offers/offers.service.ts
+++ server/src/offers/offers.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Offer } from './offer.entity';
+
+import { CreateOfferDTO } from './dto/create-offer.dto';
 
 @Injectable()
-export class OffersService {}
+export class OffersService {
+  constructor(
+    @InjectRepository(Offer)
+    private offersRepository: Repository<Offer>,
+  ) {}
+
+  async getOffers(): Promise<Offer[]> {
+    const offers = this.offersRepository.find();
+
+    return offers;
+  }
+
+  async getOfferById(id: string): Promise<Offer> {
+    const foundOffer = await this.offersRepository.findOneBy({ id });
+
+    if (!foundOffer) {
+      throw new NotFoundException(`Task with ID "${id}" not found.`);
+    }
+
+    return foundOffer;
+  }
+
+  async createOffer(createOfferDto: CreateOfferDTO): Promise<Offer> {
+    const {
+      address,
+      area,
+      description,
+      features,
+      photos,
+      price,
+      propertyType,
+      rooms,
+    } = createOfferDto;
+
+    const newOffer = this.offersRepository.create({
+      address,
+      area,
+      description,
+      features,
+      photos,
+      price,
+      propertyType,
+      rooms,
+    });
+
+    await this.offersRepository.save(newOffer);
+
+    return newOffer;
+  }
+
+  async deleteOffer(id: string): Promise<void> {
+    const result = await this.offersRepository.delete({ id });
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID "${id}" not found.`);
+    }
+  }
+}
