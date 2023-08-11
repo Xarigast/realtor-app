2993eace544b8ba5b6269d9875bf5059fac16e50
Xarigast <xarigast@gmail.com>
2023-08-11 11:44:19 +0300

    server: apply connection to db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { OffersModule } from './offers/offers.module';
 
+import { Offer } from './offers/offer.entity';
+
 @Module({
-  imports: [OffersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [Offer],
+    }),
+    OffersModule,
+  ],
   providers: [],
   controllers: [],
 })
--- server/src/offers/offers.module.ts
+++ server/src/offers/offers.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { OffersService } from './offers.service';
 import { OffersController } from './offers.controller';
 
+import { Offer } from './offer.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Offer])],
   providers: [OffersService],
   controllers: [OffersController],
 })
