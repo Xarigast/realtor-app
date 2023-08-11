a4dcf771315ed478030ca6a6e7efa2267e11fc87
Xarigast <xarigast@gmail.com>
2023-08-11 11:42:37 +0300

    server: implement offers service methods in controller

--- server/src/offers/offers.controller.ts
+++ server/src/offers/offers.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+
+import { OffersService } from './offers.service';
+
+import { Offer } from './offer.entity';
+
+import { CreateOfferDTO } from './dto/create-offer.dto';
 
 @Controller('offers')
-export class OffersController {}
+export class OffersController {
+  constructor(private offersService: OffersService) {}
+
+  @Get()
+  getOffers(): Promise<Offer[]> {
+    return this.offersService.getOffers();
+  }
+
+  @Get('/:id')
+  getOfferById(@Param('id') id: string): Promise<Offer> {
+    return this.offersService.getOfferById(id);
+  }
+
+  @Post()
+  createOffer(@Body() createOfferDto: CreateOfferDTO): Promise<Offer> {
+    return this.offersService.createOffer(createOfferDto);
+  }
+
+  @Delete('/:id')
+  deleteOffer(@Param() id: string): Promise<void> {
+    return this.offersService.deleteOffer(id);
+  }
+}
